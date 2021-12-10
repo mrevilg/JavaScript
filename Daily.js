e170fdf0bb8c4dea85db94d6b72f054206a8f41d
Stephen Clements <mrevilg@gmail.com>
2021-12-10 07:21:16 -0600

    primative

--- Daily.js
+++ Daily.js
@@ -14,3 +14,10 @@ block comment
 
 console./* even heere */log(123);
 
+console.log(20.49); //Floats don't need special treatment
+console.log(3 + 4); // Prints 7
+console.log(5 - 1); // Prints 4
+console.log(4 * 2); // Prints 8
+console.log(9 / 3); // Prints 3
+console.log(11 % 3); // Prints 2
+console.log(12 % 3); // Prints 0

0517035e5e131c0688c5ed1ab27931012b54a2e3
Stephen Clements <mrevilg@gmail.com>
2021-12-15 07:26:56 -0600

    Built-In Obj

--- Daily.js
+++ Daily.js
@@ -30,3 +30,8 @@ console.log('Hello ' + 'World'); // Prints 'Hello World'
 console.log('Teaching the world how to code'.length); // Prints string length property
 
 console.log('hello'.lastIndexOf()); // Prints -1
+
+console.log(Math.random()); // Prints a random number between 0 and 1
+console.log(Math.floor(Math.random() * 50)); // Prints a random whole number between 0 and 50
+console.log(Math.ceil(43.8)); // Prints smallest int >= decimal
+console.log(Number.isInteger(2017)); //Prints 'true'

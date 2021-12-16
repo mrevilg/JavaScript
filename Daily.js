397802a10fb94f9870cd09f417bd7ab3477ddbd1
Stephen Clements <mrevilg@gmail.com>
2021-12-16 12:23:07 -0600

    var

--- Daily.js
+++ Daily.js
@@ -35,3 +35,9 @@ console.log(Math.random()); // Prints a random number between 0 and 1
 console.log(Math.floor(Math.random() * 50)); // Prints a random whole number between 0 and 50
 console.log(Math.ceil(43.8)); // Prints smallest int >= decimal
 console.log(Number.isInteger(2017)); //Prints 'true'
+
+
+var favoriteFood = 'pizza'
+var numOfSlices = 8
+console.log(favoriteFood);
+console.log(numOfSlices);

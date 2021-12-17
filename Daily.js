8ec06e24d0c4fca1d57451c7781c29f98b727b11
Stephen Clements <mrevilg@gmail.com>
2021-12-17 07:46:11 -0600

    let

--- Daily.js
+++ Daily.js
@@ -41,3 +41,6 @@ var favoriteFood = 'pizza'
 var numOfSlices = 8
 console.log(favoriteFood);
 console.log(numOfSlices);
+
+let changeMe = true // let can be reassigned
+console.log(changeMe = false)

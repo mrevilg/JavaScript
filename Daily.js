ecf0ef57aef0075802d7563d7e914c5e66c80aed
Stephen Clements <mrevilg@gmail.com>
2021-12-19 08:10:27 -0600

    Math Assignments

--- Daily.js
+++ Daily.js
@@ -12,7 +12,7 @@ block comment
 
 */
 
-console./* even heere */log(123);
+console./* even here */log(123);
 
 console.log(20.49); //Floats don't need special treatment
 console.log(3 + 4); // Prints 7
@@ -47,3 +47,15 @@ console.log(changeMe = false)
 
 const entree = 'Enchiladas' // immutable variable
 console.log(entree);
+
+let x = 20;
+x -= 5; // Can be written as x = x - 5
+console.log(x); // Output: 15
+ 
+let y = 50;
+y *= 2; // Can be written as y = y * 2
+console.log(y); // Output: 100
+ 
+let z = 8;
+z /= 2; // Can be written as z = z / 2
+console.log(z); // Output: 4

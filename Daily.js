88a45f638425491ca30c3746d985b5bc3bbd1cc0
Stephen Clements <mrevilg@gmail.com>
2021-12-23 16:32:03 -0600

    typeof

--- Daily.js
+++ Daily.js
@@ -79,3 +79,20 @@ console.log('My favorite animal: ' + favoriteAnimal);
 var myName = 'Stephen'
 var myCity = 'London'
 console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+// typeof operator
+
+const unknown1 = 'foo';
+console.log(typeof unknown1); // Output: string
+ 
+const unknown2 = 10;
+console.log(typeof unknown2); // Output: number
+ 
+const unknown3 = true; 
+console.log(typeof unknown3); // Output: boolean
+
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+
+newVariable = 1;
+console.log(typeof newVariable);

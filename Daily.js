b0770c7eec9d4942ff2d2e976590334e5fb03ebf
Stephen Clements <mrevilg@gmail.com>
2021-12-26 10:00:27 -0600

    if else

--- Daily.js
+++ Daily.js
@@ -102,3 +102,11 @@ console.log(typeof newVariable);
 if (true) {
 console.log('This message will print!'); 
 } // Prints: This message will print!
+
+let sale = true;
+sale = false;
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}

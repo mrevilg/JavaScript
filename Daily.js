b9187bb8cacc832e38ae81dc24ee2e5239e2c380
Stephen Clements <mrevilg@gmail.com>
2021-12-27 05:00:29 -0600

    Comparative

--- Daily.js
+++ Daily.js
@@ -110,3 +110,13 @@ if(sale) {
 } else {
   console.log('Time to wait for a sale.');
 }
+
+// Compartive Operators
+
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!')
+} else {
+  console.log('We can eat later!')
+}

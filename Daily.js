c080648ce4c80d82be4be69347af4959b0d92959
Stephen Clements <mrevilg@gmail.com>
2021-12-31 09:50:06 -0600

    switch

--- Daily.js
+++ Daily.js
@@ -126,6 +126,26 @@ if (season === 'spring') {
   console.log('Invalid season.');
 }
 
+// Swich kewyord - multi else/if algo
+
+let athleteFinalPosition = 'first place';
+
+switch(athleteFinalPosition){
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
+
+
 
 // Compartive Operators
 

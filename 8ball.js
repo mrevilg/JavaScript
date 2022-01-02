6eb07985f1a6a5b6ce799197066a568cd9e4bf87
Stephen Clements <mrevilg@gmail.com>
2022-01-02 09:13:37 -0600

    8ball

---
+++ 8ball.js
@@ -0,0 +1,12 @@
+// Magic 8 Ball Project
+
+
+// define a variable called userName that is set to an empty string.
+let userName = '';
+
+// create a ternary expression that decides what to do if the user enters a name or not.
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
+
+/*
+Comment Block
+*/

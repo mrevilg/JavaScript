7c769259651ff0e4871cf678f910184e373459f9
Stephen Clements <mrevilg@gmail.com>
2022-01-02 09:29:27 -0600

    userQuestion

--- 8ball.js
+++ 8ball.js
@@ -7,6 +7,11 @@ let userName = '';
 // create a ternary expression that decides what to do if the user enters a name or not.
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
 
+// Create a variable named userQuestion. The value of the variable should be a string that is the question
+
+const userQuestion = 'What is your question for the great magic ball?!'
+
+
 /*
 Comment Block
 */

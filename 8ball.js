94315dd5ce0b5c9622a3e5c88f1317f53854cde6
Stephen Clements <mrevilg@gmail.com>
2022-01-04 07:26:34 -0600

    control flow

--- 8ball.js
+++ 8ball.js
@@ -19,6 +19,40 @@ var randomNumber = Math.floor(Math.random() * 8);
 // Create one more variable named eightBall, and set it equal to an empty string.
 
 let eightBall ='';
+
+// create a control flow.
+
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+    case 4:
+        eightBall = 'Do not count on it';
+        break;
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+}
+
+// print the Magic Eight Ball’s answer
+
+console.log(`The Magic 8 Ball answered: ${eightBall}`);
+
 /*
 Comment Block
 */

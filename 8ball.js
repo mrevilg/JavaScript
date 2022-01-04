4f71a631799bd2f5ac39fe3ee10a2131b733b2e0
Stephen Clements <mrevilg@gmail.com>
2022-01-04 07:15:24 -0600

    8ball var

--- 8ball.js
+++ 8ball.js
@@ -5,17 +5,20 @@
 let userName = '';
 
 // create a ternary expression that decides what to do if the user enters a name or not.
-userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!')
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
 // Create a variable named userQuestion. The value of the variable should be a string that is the question
-const userQuestion = 'What is your question for the great magic ball?!'
+const userQuestion = 'What is your question for the great magic ball?!';
 
 // Write a console.log() for the userQuestion, stating what was asked. 
-console.log(`${userName} asked: ${userQuestion}`)
+console.log(`${userName} asked: ${userQuestion}`);
 
 // We need to generate a random number between 0 and 7.
 var randomNumber = Math.floor(Math.random() * 8);
 
+// Create one more variable named eightBall, and set it equal to an empty string.
+
+let eightBall ='';
 /*
 Comment Block
 */

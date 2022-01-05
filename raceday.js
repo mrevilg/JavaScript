9346b8eedbf4f94c91d25bcd77fca4c6dba9a81a
Stephen Clements <mrevilg@gmail.com>
2022-01-05 08:28:11 -0600

    raceday math

---
+++ raceday.js
@@ -0,0 +1,7 @@
+// Race Day Registration Project
+
+// Randomized number assignment (inclusive)
+let raceNumber = Math.floor(Math.random() * 1000);
+
+// Variable assigned with bool value
+let registeredEarly = false;

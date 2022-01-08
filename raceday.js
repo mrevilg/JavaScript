e0d3180561a1a618a5bd3fb78985257bab0ecd8f
Stephen Clements <mrevilg@gmail.com>
2022-01-08 09:28:52 -0600

    final

--- raceday.js
+++ raceday.js
@@ -23,6 +23,6 @@ if (registeredEarly && age > 18) {
     console.log(`Race will begin at 11:00, your race number is ${raceNumber}.`);
 }   else if (age < 18){
     console.log(`Race will begin at 12:30, your race number is ${raceNumber}.`);
-}   else (age == 18 ){
+}   else { 
     console.log(`Go to tht eregistration desk.`);
 }

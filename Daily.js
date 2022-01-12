5e9e47e319eb89e034fa8440ab647680663e7cf2
Stephen Clements <mrevilg@gmail.com>
2022-01-12 08:13:12 -0600

    default parameters

--- Daily.js
+++ Daily.js
@@ -240,3 +240,17 @@ const rectWidth = 10;
 const rectHeight = 6;
 
 calculateArea(rectWidth, rectHeight);
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+
+sayThanks('Cole')
+
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+} // Default parameters
+
+makeShoppingList();

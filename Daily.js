ad018c5f24ad30e770069753cdf307bbd3de5d39
Stephen Clements <mrevilg@gmail.com>
2022-01-13 14:27:53 -0600

    return function

--- Daily.js
+++ Daily.js
@@ -254,3 +254,13 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
 } // Default parameters
 
 makeShoppingList();
+
+// Function Returns
+
+function monitorCount(rows, columns){
+  return (rows * columns);
+}
+
+const numOfMonitors = (monitorCount(5, 4));
+
+console.log(numOfMonitors);

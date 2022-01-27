5dedd8f785ea588d755872931d27ac850f818e02
Stephen Clements <mrevilg@gmail.com>
2022-01-27 07:58:24 -0600

    Element_updates

--- Daily.js
+++ Daily.js
@@ -380,3 +380,9 @@ console.log(listItem);
 
 console.log(famousSayings[2]); // logs to screen element three
 console.log(famousSayings[3]); // error/undefined
+
+// Element Updates
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados'; // 2nd Element aka 1st Indeces updated
+console.log(groceryList);

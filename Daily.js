684c864aca149b9bc2561c94074b8f916fdfe4f0
Stephen Clements <mrevilg@gmail.com>
2022-02-07 09:49:01 -0600

    Nested_loops_example

--- Daily.js
+++ Daily.js
@@ -485,8 +485,14 @@ const vacationSpots = ['Bali', 'Paris', 'Tulum'];
 
 // Nested Loops
 
-const bobsFollowers = ['Fred', 'Smithers', 'John', 'Frank'];
+let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
 
-var tinasFollowers = ['Fred', 'Smithers', 'Lemmie'];
-
-var mutualFollowers = [];
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+};

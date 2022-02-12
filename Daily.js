0ca2b07b0ed0c6d1f5e49392c3d238cb850deda3
Stephen Clements <mrevilg@gmail.com>
2022-02-12 09:39:05 -0600

    Do..While_Ex

--- Daily.js
+++ Daily.js
@@ -510,5 +510,9 @@ while (currentCard != 'spade') {
 // Do..While
 
 let cupsOfSugarNeeded = 3;
-
 let cupsAdded = 0;
+
+do {
+ cupsAdded++
+ console.log(cupsAdded + ' cup was added') 
+} while (cupsAdded < cupsOfSugarNeeded);

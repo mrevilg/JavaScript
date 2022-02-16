ae15d3350a2544d738686610747a374069c728c8
Stephen Clements <mrevilg@gmail.com>
2022-02-16 07:46:29 -0600

    abstraction

--- Daily.js
+++ Daily.js
@@ -529,3 +529,17 @@ for (let i = 0; i < rapperArray.length; i++){
 }
 
 console.log("And if you don't know, now you know.");
+
+// Abstraction High Order
+
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+}
+
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+
+is2p2();

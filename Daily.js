2ad117757674f5146225a00d1380675518c1980f
Stephen Clements <mrevilg@gmail.com>
2022-02-18 07:59:28 -0600

    Functions as Parameters

--- Daily.js
+++ Daily.js
@@ -544,3 +544,26 @@ const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
 
 is2p2();
 console.log(is2p2.name);
+
+// Function as Parameters
+
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+const addTwo = num => num + 2;
+
+const timeFuncRuntime = funcParameter => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
+
+// Write your code below
+
+const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);

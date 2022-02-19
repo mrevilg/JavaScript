9a94fe6547222f4835ceb99be003e9a6ba17e442
Stephen Clements <mrevilg@gmail.com>
2022-02-19 08:40:41 -0600

    Functions as Parameters

--- Daily.js
+++ Daily.js
@@ -567,3 +567,15 @@ const timeFuncRuntime = funcParameter => {
 // Write your code below
 
 const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+
+const checkConsistentOutput = (func, val) => {
+    let firstTry = func(val);
+    let secondTry = func(val);
+    if (firstTry === secondTry) {
+        return firstTry
+    } else {
+        return 'This function returned inconsistent results'
+    }
+};
+
+checkConsistentOutput(addTwo, 10);

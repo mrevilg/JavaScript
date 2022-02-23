700e4fcea2045a70e758cf9f4c4d7a071bd28c4c
Stephen Clements <mrevilg@gmail.com>
2022-02-23 08:02:49 -0600

    .map()

--- Daily.js
+++ Daily.js
@@ -602,9 +602,20 @@ const onlyNumbers = things.filter(thing => {
 
 console.log(onlyNumbers);
 
-// .forEaahc()
+// .forEach()
 
 const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterates over fruits below
 fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`))
+
+// .map()
+
+const numbers = [1, 2, 3, 4, 5]; 
+ 
+const bigNumbers = numbers.map(number => {
+  return number * 10;
+});
+
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
+console.log(bigNumbers); // Output: [10, 20, 30, 40, 50]
